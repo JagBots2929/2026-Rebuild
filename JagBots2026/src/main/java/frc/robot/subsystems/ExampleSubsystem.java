0bd3f9cc28a75b477f4f4b14c1805977d3661b0b
xuxnx <enzobu4@icloud.com>
2026-01-21 17:24:08 -0800

    Revert "I made a comment"
    This reverts commit 1c8e608246d86249f347ac91de081435704feb54.

--- JagBots2026/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ JagBots2026/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -38,9 +38,7 @@ public class ExampleSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    // this is a comment
   }
-
 
   @Override
   public void simulationPeriodic() {
